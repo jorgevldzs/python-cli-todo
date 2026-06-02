ee47d815d30ffd7f77075b34031f9d457d76fe32
Jorge Valdez <jorgearnoldovs@gmail.com>
2026-06-02 16:06:54 -0700

    Version 1

--- todo.py
+++ todo.py
@@ -2,9 +2,21 @@ import sys
 
 def main():
     if len(sys.argv) < 2:
-        sys.exit("You need to add a command")
+        print("For running this program you need to use the command line\n")
+        print("You should use the following command: ")
+        print("\tpython todo.py [command]\n")
+        print("The available commands are:")
+        print("\tadd - Adds a task. Prompts user for task text.")
+        print("\tdone - Deletes a task. Prompts user for task id.")
+        print("\ttasks - Displays all inputted tasks.")
+
+        sys.exit()
     elif len(sys.argv) > 2:
-        sys.exit("Too many arguments")
+        print("Too many arguments\n")
+        print("You should use the following command: ")
+        print("\tpython todo.py [command]\n")
+
+        sys.exit()
     else:
         match sys.argv[1]:
             case "add":
@@ -13,13 +25,21 @@ def main():
                 done()
             case "tasks":
                 tasks()
-            case "--help" | "-h":
-                print("Help")
             case _:
-                sys.exit("That's and invalid command")
+                print("That's and invalid command\n")
+                print("The available commands are:")
+                print("\tadd - Adds a task. Prompts user for task text.")
+                print("\tdone - Deletes a task. Prompts user for task id.")
+                print("\ttasks - Displays all inputted tasks.")
+
+                sys.exit()
 
 def add():
     task = input("Task: ")
+
+    if "±±±" in task:
+        sys.exit("Invalid task format. Try again")
+
     indexes = []
     lines = []
 
@@ -45,9 +65,10 @@ def add():
 
             last_index = int(indexes.pop(len(indexes) - 1))
             file.write(str(last_index + 1) + "±±±" + task + "\n")
+        
+            print(f"You have added the task: {task}")
 
 def done():
-    id_task = input("Task to mark as done (ID): ")
     indexes = []
     lines = []
 
@@ -55,8 +76,12 @@ def done():
         for line in file:
             lines.append(line.removesuffix("\n"))
     
+    if lines[0] == "0":
+        sys.exit("You don't have tasks to do")
+    else:
 
-    if lines[0] != "0":
+        id_task = input("Task to mark as done (ID): ")
+
         for line in lines[1:]:
             num, todo = line.split("±±±")
             indexes.append(num)
@@ -73,6 +98,10 @@ def done():
                 
                 if i != location_task:
                     file.write(lines[i + 1] + "\n")
+                else:
+                    task = lines[i + 1]
+
+            print(f"You have marked as done the task: {task.removeprefix(f"{id_task}±±±")}")
     else:
         sys.exit("Any task has that ID")
 
@@ -83,11 +112,14 @@ def tasks():
         for line in file:
             lines.append(line.removesuffix("\n"))
     
-    print(f"You have {lines[0]} tasks to do:")
+    if lines[0] == "0":
+        sys.exit("You don't have tasks to do")
+    else:
+        print(f"You have {lines[0]} tasks to do:\n")
 
-    for line in lines[1:]:
-        num, task = line.split("±±±")
-        print(f"ID: {num} - {task}")
+        for line in lines[1:]:
+            num, task = line.split("±±±")
+            print(f"ID: {num} - {task}")
 
 if __name__ == "__main__":
     main()
